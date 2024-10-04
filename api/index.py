8419fccb78011e24161e99fe96a2761096b01250
JCharls1 <151930727+JCharls1@users.noreply.github.com>
2024-10-04 23:35:01 +0800

    wdwd

--- api/index.py
+++ api/index.py
@@ -1,14 +1,10 @@
-# /api/index.py
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request  # Added 'request'
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return "Flask Vercel Example - Hello World", 200
-
 
 @app.errorhandler(404)
 def page_not_found(e):
